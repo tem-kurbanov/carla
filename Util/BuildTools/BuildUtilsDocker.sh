15c5d57e2dc0f501178eaefc3f8cc17c5906b636
Temirlan Kurbanov <72080725+tem-kurbanov@users.noreply.github.com>
2024-05-14 13:33:43 +0200

    changed wget to copy from file

--- Util/BuildTools/BuildUtilsDocker.sh
+++ Util/BuildTools/BuildUtilsDocker.sh
@@ -17,7 +17,8 @@ FBXSDK_URL=https://www.autodesk.com/content/dam/autodesk/www/adn/fbx/2020-0-1/${
 
 if [ ! -d "${FBX2OBJ_DEP_FOLDER}" ]; then
   log "Downloading FBX SDK..."
-  wget -c "${FBXSDK_URL}" -P "${CARLA_DOCKER_UTILS_FOLDER}" --user-agent="Mozilla"
+  # wget -c "${FBXSDK_URL}" -P "${CARLA_DOCKER_UTILS_FOLDER}" --user-agent="Mozilla"
+  mv /home/carla/fbx202001_fbxsdk_linux.tar.gz "${CARLA_DOCKER_UTILS_FOLDER}"
 
   echo "Unpacking..."
   mkdir -p "${FBX2OBJ_DEP_FOLDER}"
